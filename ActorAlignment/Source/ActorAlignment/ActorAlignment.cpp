4ea119091100f0fda01a26fd38fa7b1d60d97f00
Npbrodersen <npbrodersen@web.de>
2015-10-19 18:54:58 +0200

    ADD Project Data

---
+++ ActorAlignment/Source/ActorAlignment/ActorAlignment.cpp
@@ -0,0 +1,7 @@
+// Copyright 1998-2015 Epic Games, Inc. All Rights Reserved.
+
+#include "ActorAlignment.h"
+
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, ActorAlignment, "ActorAlignment" );
+ 
---
+++ ActorAlignment/Source/ActorAlignment/ActorAlignmentGameMode.h
@@ -0,0 +1,16 @@
+// Copyright 1998-2015 Epic Games, Inc. All Rights Reserved.
+#pragma once
+#include "GameFramework/GameMode.h"
+#include "ActorAlignmentGameMode.generated.h"
+
+UCLASS(minimalapi)
+class AActorAlignmentGameMode : public AGameMode
+{
+	GENERATED_BODY()
+
+public:
+	AActorAlignmentGameMode();
+};
+
+
+
